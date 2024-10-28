db00d56902384847873ec546bcb507e99624f8cf
mizdra <pp.mizdra@gmail.com>
2024-10-29 01:03:31 +0900

    handle base path in middleware

--- middleware.tsx
+++ middleware.tsx
@@ -1,5 +1,13 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function middleware() {
-  return NextResponse.next();
+export function middleware(req: NextRequest) {
+  if (
+    req.nextUrl.pathname === '/base' ||
+    req.nextUrl.pathname.startsWith('/base/')
+  ) {
+    const newUrl = new URL(req.nextUrl.pathname.replace('/base', ''), req.nextUrl.origin);
+    return NextResponse.rewrite(newUrl);
+  } else {
+    return NextResponse.next();
+  }
 }
